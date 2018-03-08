712ba5f300c37a589094a72dac9c65c04fda3adf
MinSu Kim <net94@naver.com>
2018-03-08 12:38:54 +0900

    assertion 완료

---
+++ src/main/java/introduce_assertion/after_add/Main.java
@@ -0,0 +1,30 @@
+package introduce_assertion.after_add;
+
+import java.util.Random;
+
+public class Main {
+	private static final Random rnd = new Random(1234);
+	
+	public static void main(String[] args) {
+		execute(10);
+		execute(10);
+		execute(10);
+		execute(10);
+		execute(10);
+	}
+
+	private static void execute(int length) {
+		int[] data = new int[length];
+		for(int i=0; i<data.length; i++) {
+			data[i] = rnd.nextInt(data.length);
+		}
+		
+		SortSample sorter = new SortSample(data);
+		System.out.printf("%6s : %s%n", "BEFORE" ,sorter);
+		
+		sorter.sort();
+		System.out.printf("%6s : %s%n", "AFTER" ,sorter);
+		
+		System.out.println();
+	}
+}
---
+++ src/main/java/introduce_assertion/after_add/SortSample.java
@@ -0,0 +1,70 @@
+package introduce_assertion.after_add;
+
+public class SortSample {
+	private final int[] data;
+	
+	private static boolean ASSERT = true;
+	
+	public SortSample(int[] data) {
+		this.data = new int[data.length];
+		System.arraycopy(data, 0, this.data, 0, data.length);
+	}
+	
+	/**
+	 * 선택 정렬
+	 */
+	public void sort() {
+		for(int x=0; x <data.length - 1; x++) {
+			int m = x;
+			for(int y = x + 1; y <data.length; y++) {
+				if (data[m] > data[y]) {
+					m = y;
+				}
+			}
+			
+			if (ASSERT) {
+				assert isMin(m, x, data.length-1) : this + ", x = " + x;
+			}
+			int v = data[m];	
+			data[m] = data[x];
+			data[x] = v;
+			
+			if (ASSERT) {
+				assert isSorted(0, x+1) : this + ", x = " + x;
+			}
+			
+		}
+	}
+
+	private boolean isMin(int pos, int start, int end) {
+		for (int i=start ; i<end+1; i++) {
+			if (data[pos] > data[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private boolean isSorted(int start, int end) {
+		for (int i=start ; i<end; i++) {
+			if (data[i] > data[i+1]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("[ ");
+		for(int i=0; i <data.length; i++) {
+			buffer.append(data[i]);
+			buffer.append(",");
+		}
+		int start = buffer.lastIndexOf(",");
+		buffer.replace(start, start+1, "");
+		buffer.append(" ]");
+		return buffer.toString();
+	}
+}
