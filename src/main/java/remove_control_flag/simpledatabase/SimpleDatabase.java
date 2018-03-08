7da0ffe777ec0620529b92acfd35e3fa91500cf1
MinSu Kim <net94@naver.com>
2018-03-08 11:27:49 +0900

    refactoring 적용

--- src/main/java/remove_control_flag/simpledatabase/SimpleDatabase.java
+++ src/main/java/remove_control_flag/simpledatabase/SimpleDatabase.java
@@ -9,48 +9,27 @@ import java.util.Map;
 
 public class SimpleDatabase {
 	private Map<String, String> map = new HashMap<>();
-	
+
 	public SimpleDatabase(Reader r) throws IOException {
 		BufferedReader reader = new BufferedReader(r);
-		boolean reading = false;
-		
 		String line;
-		while(!reading) {
-			line = reader.readLine();
-			if (line == null) {
-				reading = true;
-			}else {
-				boolean scanningKey = true;
-				StringBuffer keyBuffer = new StringBuffer();
-				StringBuffer valueBuffer = new StringBuffer();
-				for (int i = 0; i <line.length(); i++) {
-					char c = line.charAt(i);
-					if (scanningKey) {
-						if (c == '=') {
-							scanningKey = false;
-						}else {
-							keyBuffer.append(c);
-						}
-					}else {
-						valueBuffer.append(c);
-					}
-				}
-				String key = keyBuffer.toString();
-				String value = valueBuffer.toString();
-				map.put(key, value);
-			}
+		while ((line = reader.readLine()) != null) {
+			int equalIndex = line.indexOf("=");
+			String key = line.substring(0, equalIndex);
+			String value = line.substring(equalIndex+1);
+			map.put(key, value);
 		}
 	}
 
 	public void putValue(String key, String value) {
 		map.put(key, value);
 	}
-	
+
 	public String getValue(String key) {
 		return map.get(key);
 	}
-	
-	public Iterator<String> iterator(){
+
+	public Iterator<String> iterator() {
 		return map.keySet().iterator();
 	}
 }
