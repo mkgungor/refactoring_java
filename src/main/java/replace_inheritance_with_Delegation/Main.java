88459771d8d81d90a3a741144598575488456e68
MinSu-Kim <net94.teacher73@gmail.com>
2018-03-21 23:13:13 +0900

    위임 완료

---
+++ src/main/java/replace_inheritance_with_Delegation/Main.java
@@ -0,0 +1,24 @@
+package replace_inheritance_with_Delegation;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Dice> dices = Arrays.asList(
+				new Dice(),
+				new Dice(456L),
+				new Dice()
+				);
+		dices.get(2).setSeed(456L);
+		
+		for(Dice d: dices) {
+			for (int i=0; i<10; i++) {
+				System.out.printf("%d%s ",d.nextInt(), i<9?",":"");
+			}
+			System.out.println();
+		}
+	}
+
+}
